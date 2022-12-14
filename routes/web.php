21d992d3df3ef6e3568b26020c4dff850ffd7215
tomasrl18 <tomaslopezraigal@gmail.com>
2022-12-14 12:26:14 +0100

    Videos 1, 2 y 3. Hasta rutas

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+Route::get('/', function () {
+    return 'Home';
+});
+
+Route::get('/usuarios', function () {
+    return 'Usuarios';
+});
+
+Route::get('/usuarios/nuevo', function () {
+    return 'Crear nuevo usuario';
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+    return 'Mostrando detalles del usuario: ' . $id;
+});
+
+Route::get('/saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+    if ($nickname) {
+        return 'Bienvenido ' . $name . ' tu apodo es ' . $nickname;
+    } else {
+        return 'Bienvenido ' . $name . ', no tienes apodo.';
+    }
+});
