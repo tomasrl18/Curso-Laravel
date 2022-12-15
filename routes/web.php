ce581c48a2592ec3a98e2fa2839585ac02ef1c13
tomasrl18 <tomaslopezraigal@gmail.com>
2022-12-15 10:18:23 +0100

    Video 5

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,13 @@
 <?php
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
-
 Route::get('/', function () {
     return 'Home';
 });
 
-Route::get('/usuarios', function () {
-    return 'Usuarios';
-});
+Route::get('/usuarios', 'UserController@index');
 
-Route::get('/usuarios/nuevo', function () {
-    return 'Creando nuevo usuario';
-});
+Route::get('/usuarios/nuevo', 'UserController@create');
 
-Route::get('/usuarios/{id}', function ($id) {
-    return 'Mostrando detalles del usuario: ' . $id;
-});
+Route::get('/usuarios/{id}', 'UserController@show');
 
-Route::get('/saludo/{name}/{nickname?}', function ($name, $nickname = null) {
-    $name = ucfirst($name);
-
-    if ($nickname) {
-        return 'Bienvenido ' . $name . ' tu apodo es ' . $nickname . '.';
-    } else {
-        return 'Bienvenido ' . $name . '.';
-    }
-});
+Route::get('/saludo/{name}/{nickname?}', 'WelcomeUserController');
